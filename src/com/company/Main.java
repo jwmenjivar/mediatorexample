4822311022ac36533a358aab217d92a232b87292
jwmenjivar <jwmenjivar@gmail.com>
2022-01-14 04:21:23 -0600

    added main class, exercise working properly

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	StockMediator nyse = new StockMediator();
+
+    GormanSlacks broker = new GormanSlacks(nyse);
+    JTPoorman broker2 = new JTPoorman(nyse);
+
+    broker.saleOffer("MSFT", 100);
+    broker.saleOffer("GOOG", 50);
+
+    broker2.buyOffer("MSFT", 100);
+    broker2.saleOffer("NRG", 10);
+
+    broker.buyOffer("NRG", 10);
+
+    nyse.getStockOfferings();
+
     }
 }
